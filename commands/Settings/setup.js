19b893a45d1654f2d94d9965174cf80b7992f866
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-01-09 22:50:16 -0600

    👋 almost finished setup system
    although it is still missing the database system, it is almost working with all 3 buttons

--- commands/Settings/setup.js
+++ commands/Settings/setup.js
@@ -1,4 +1,9 @@
-const { MessageActionRow, MessageButton, Permissions, MessageEmbed } = require("discord.js");
+const {
+  MessageActionRow,
+  MessageButton,
+  Permissions,
+  MessageEmbed,
+} = require("discord.js");
 
 module.exports = {
   name: "setup",
@@ -13,35 +18,91 @@ module.exports = {
    * @returns
    */
   run: async (rainy, message, args) => {
-      if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-          message.reply('sorry! this command can only be ran by server admins!')
-      } else {
-        const btnRow = new MessageActionRow().addComponents(
-            new MessageButton()
-              .setCustomId("funBtn")
-              .setLabel("🥳Fun & Games🎮")
-              .setStyle("PRIMARY"),
-            new MessageButton()
-              .setCustomId("modBtn")
-              .setDisabled(true)
-              .setLabel("🚔Moderation🛠️")
-              .setStyle("PRIMARY"),
-            new MessageButton()
-              .setCustomId("gaBtn")
-              .setDisabled(true)
-              .setLabel("🎉Giveaway🎟️")
-              .setStyle("PRIMARY")
-          );
+    if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      message.reply("sorry! this command can only be ran by server admins!");
+    } else {
+      const btnRow = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("funBtn")
+          .setDisabled(false)
+          .setLabel("🥳Fun & Games🎮")
+          .setStyle("PRIMARY"),
+        new MessageButton()
+          .setCustomId("modBtn")
+          .setDisabled(false)
+          .setLabel("🚔Moderation🛠️")
+          .setStyle("PRIMARY"),
+        new MessageButton()
+          .setCustomId("gaBtn")
+          .setDisabled(true)
+          .setLabel("🎉Giveaway🎟️")
+          .setStyle("PRIMARY")
+      );
 
-          const descEmbed = new MessageEmbed()
-          .setTitle('⛈️rainy setup⛈️')
-          .setDescription('hey! please use the following buttons that have server topics on then, then choose which one your server falls under, then i will auto generate settings for your server! *dont worry! you can always edit settings by running: `settings.`')
-          .setColor('#36393F')
-          .setFooter('💖made with love -tyler :)')
-          .setTimestamp()
+      const finishedRow = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setLabel("Visit Website!")
+          .setURL("https://bitly.com/98K8eH")
+          .setStyle("LINK")
+      );
 
-          message.reply({ embeds: [descEmbed], components: [btnRow] })
-          
-      }
+      const descEmbed = new MessageEmbed()
+        .setTitle("⛈️rainy setup⛈️")
+        .setDescription(
+          "hey! please use the following buttons that have bot topics on then, then choose which one you want the bot to be, then i will auto generate settings for your server! *dont worry! you can always edit settings by running: `'settings`"
+        )
+        .setColor("#36393F")
+        .setFooter("💖made with love -tyler :)")
+        .setTimestamp();
+
+      const funClickedEmbed = new MessageEmbed()
+        .setTitle("⛈️rainy setup⛈️")
+        .setDescription(
+          "the settings that were changed/added/removed can be viewed down below"
+        )
+        .addFields({ name: "server type:", value: "🥳Fun & Games🎮" })
+        .setColor("GREEN")
+        .setFooter("💖made with love -tyler :)")
+        .setTimestamp();
+
+      const modClickedEmbed = new MessageEmbed()
+        .setTitle("⛈️rainy setup⛈️")
+        .setDescription(
+          "the settings that were changed/added/removed can be viewed down below"
+        )
+        .addFields({ name: "server type:", value: "🚔Moderation🛠️" })
+        .setColor("GREEN")
+        .setFooter("💖made with love -tyler :)")
+        .setTimestamp();
+
+      message.reply({
+        embeds: [descEmbed],
+        components: [btnRow],
+        ephemeral: true,
+      });
+
+      const filter = (i) => ["funBtn", "modBtn", "gaBtn"].includes(i.customId);
+      const collector = message.channel.createMessageComponentCollector({
+        filter,
+        max: 1,
+        time: 1000 * 15,
+      });
+
+      collector.on("collect", async (i) => {
+        if (i.customId === "funBtn") {
+          await i.update({
+            embeds: [funClickedEmbed],
+            components: [finishedRow],
+          });
+        } else {
+          if (i.customId === "modBtn") {
+            await i.update({
+              embeds: [modClickedEmbed],
+              components: [finishedRow],
+            })
+          }
+        }
+      });
+    }
   },
 };
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -13,7 +13,7 @@ rainy.on("guildCreate", async (guild) => {
     `hey, thanks for inviting me to your server. i will walk you through the setup process!
      \n \n1. join the support server in case you have any difficulty: ${rainyConfig.serverInvite}
      \n2. go to your mod channel.
-     \n3. run the command: 'setup , this will send buttons with server topics for you do choose which one your server falls under, then i will auto generate settings for your server! *dont worry! you can always edit settings by running: 'settings.
+     \n3. run the command: 'setup , please use the following buttons that have bot topics on then, then choose which one you want the bot to be, then i will auto generate settings for your server! *dont worry! you can always edit settings by running: 'settings.
      \n3. after this you are... almost done! please view the help command by running: 'help
      \n4. ok fine ill let you go, but you need to drag my premade role called "rainy -app-"
      \n\nwait!!! would you do me a favor and leave a star on our github page? ${rainyConfig.githubLink}`
