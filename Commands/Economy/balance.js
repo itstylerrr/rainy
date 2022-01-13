cee5718bd62142afa97125bfa777dc911f4d79aa
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-01-12 21:01:21 -0600

    🪙 added economy model
    added balance command

---
+++ Commands/Economy/balance.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: "balance",
+    usage: ["Check your balance for the server economy."],
+    enabled: true,
+    aliases: ["bal"],
+    category: "Economy",
+    memberPermissions: [],
+    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+    //Settings for command
+    nsfw: false,
+    ownerOnly: false,
+    cooldown: 5000,
+
+    // Execute contains content for the command
+    async execute(client, message, args, data){
+        if(args[0]) return message.reply(`Sorry! But at this moment you are only allowed to check your own balance (the IRS told us to do this) HOWEVER, you are able to do !richest to see who the most bouge.`)
+
+        const balEmbed = new MessageEmbed()
+        .setTitle('💵 Your Account 💵')
+        .addFields(
+          {name: '📤 Wallet', value: `💵 ${data.user.cash}`, inline: true},
+          {name: '🏦 Bank', value: `💵 ${data.user.bank}`, inline: true}
+        )
+        .setColor(message.guild.me.displayHexColor)
+        .setThumbnail('https://freepngimg.com/download/dollar/64058-united-dollar-sign-states-design-icon.png')
+        .setFooter(`Your assets in ${message.guild.name} || Economy by rainy *'invite*`)
+
+        message.reply({ embeds: [balEmbed] })
+    }
+}
--- Database/Schema/User.js
+++ Database/Schema/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model("User", new mongoose.Schema({
     id: { type: String },
     registeredAt: { type: Number, default: Date.now() },
-    coins: { type: Number, default: 1000 },
+    cash: { type: Number, default: 1000 },
     bank: { type: Number },
     workcd: { type: Number},
 }));
