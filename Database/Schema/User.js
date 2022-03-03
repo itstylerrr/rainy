84c5e8d660275780774274f717c6a92d59c95b8b
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-03-03 07:34:18 -0600

    🛠️ making new work system

--- Database/Schema/User.js
+++ Database/Schema/User.js
@@ -6,4 +6,16 @@ module.exports = mongoose.model("User", new mongoose.Schema({
     cash: { type: Number, default: 1000 },
     bank: { type: Number, default: 0 },
     workcd: { type: Number},
+    items: { type: Object, default: {
+        pistol: false,
+        ammo: false,
+        phone: false,
+        stock: false,
+        computer: false,
+        fishingRod: false,
+        ideLicense: false,
+        server: false,
+        
+
+    } }
 }));
