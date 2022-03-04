bc1a8932fb3dbd610016f8f664485cc42ac774d2
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-03-03 20:49:35 -0600

    Update User.js

--- Database/Schema/User.js
+++ Database/Schema/User.js
@@ -5,7 +5,8 @@ module.exports = mongoose.model("User", new mongoose.Schema({
     registeredAt: { type: Number, default: Date.now() },
     cash: { type: Number, default: 1000 },
     bank: { type: Number, default: 0 },
-    workcd: { type: Number},
+    job: { type: Number, default: 0 },
+    workcd: { type: Number },
     items: { type: Object, default: {
         pistol: false,
         ammo: false,
