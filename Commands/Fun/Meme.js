935bcee1b4c4e0a40dcd376b2429bdb4743450fb
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-05-23 19:45:11 -0500

    😁 add err logging to meme cmd

--- Commands/Fun/Meme.js
+++ Commands/Fun/Meme.js
@@ -1,30 +1,78 @@
-const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch');
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
-    name: "meme",
-    usage: ["Get meme from a random subreddit```{prefix}meme```"],
-    enabled: true,
-    aliases: ["memes"],
-    category: "Fun",
-    memberPermissions: [],
-    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-    //Settings for command
-    nsfw: false,
-    ownerOnly: false,
-    cooldown: 5000,
+  name: "meme",
+  usage: ["Get meme from a random subreddit```{prefix}meme```"],
+  enabled: true,
+  aliases: ["memes"],
+  category: "Fun",
+  memberPermissions: [],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  //Settings for command
+  nsfw: false,
+  ownerOnly: false,
+  cooldown: 5000,
 
-    // Execute contains content for the command
-    async execute(client, message, args, data){
-        let res = await fetch('https://meme-api.herokuapp.com/gimme');
-        res = await res.json();
-        const embed = new MessageEmbed()
-            .setAuthor(`Subreddit: ${res.title}`)
-            .setTitle(`🤣 Meme 🤣`)
-            .setImage(res.url)
-            .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
-            .setColor(message.guild.me.displayHexColor);
-        message.channel.send({ embeds: [embed] });
+  // Execute contains content for the command
+  async execute(client, message, args, data) {
+    try {
+      let res = await fetch("https://meme-api.herokuapp.com/gimme");
+      res = await res.json();
+      const embed = new MessageEmbed()
+        .setAuthor(`Subreddit: ${res.title}`)
+        .setTitle(`🤣 Meme 🤣`)
+        .setImage(res.url)
+        .setFooter(
+          message.member.displayName,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .setTimestamp()
+        .setColor(message.guild.me.displayHexColor);
+      message.channel.send({ embeds: [embed] });
+    } catch (err) {
+      client.logger.error(`Ran into an error while executing ${data.cmd.name}`);
+      console.log(err);
+      const { WebhookClient, MessageEmbed } = require("discord.js");
+      const { Webhooks } = require("../../config.json");
+      const currentDate = new Date();
+      const keygen = require("keygen");
+      const errKey = keygen.url(10);
+      const errorLog = new WebhookClient({
+        url: Webhooks.errors,
+      });
+      const devEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(`**Error:**\n\n${err}\n`)
+        .addFields(
+          { name: "Command:", value: data.cmd.name },
+          { name: "Error Key:", value: `\`${errKey}\`` },
+          {
+            name: "Guild:",
+            value: `Name: ${message.guild.name} | ID: ${message.guild.id}`,
+            inline: true,
+          },
+          {
+            name: "Author:",
+            value: `Name: ${message.author.tag} | ID: ${message.author.id}`,
+            inline: true,
+          },
+          {
+            name: "Created:",
+            value: `<t:${parseInt(message.createdTimestamp / 1000)}:R>`,
+          }
+        )
+        .setColor("RED");
+
+      const userEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(
+          `An error has occured running this command. Please DM <@${ownerid}> with the following error key: \`${errKey}\``
+        )
+        .setColor("RED");
+      message.reply({ embeds: [userEmbed] });
+      errorLog.send({ embeds: [devEmbed] });
+      return;
     }
-}
+  },
+};
