7903fe1c303c14f48b9a5af8648d3afcbb73f57f
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-05-23 19:38:18 -0500

    🪙 add err logging to richest cmd

--- Commands/Economy/richest.js
+++ Commands/Economy/richest.js
@@ -16,60 +16,64 @@ module.exports = {
 
   // Execute contains content for the command
   async execute(client, message, args, data) {
-    const top10 = await User.find({}).sort({ coins: -1 }).limit(10);
-    const rich1t = top10[0]
-      ? Math.floor(top10[0].bank + top10[0].cash)
-      : "nothing!";
-    const rich2t = top10[1]
-      ? Math.floor(top10[1].bank + top10[1].cash)
-      : "nothing!";
-    const rich3t = top10[2]
-      ? Math.floor(top10[2].bank + top10[2].cash)
-      : "nothing!";
-    const rich4t = top10[3]
-      ? Math.floor(top10[3].bank + top10[3].cash)
-      : "nothing!";
-    const rich5t = top10[4]
-      ? Math.floor(top10[4].bank + top10[4].cash)
-      : "nothing!";
-    const rich6t = top10[5]
-      ? Math.floor(top10[5].bank + top10[5].cash)
-      : "nothing!";
-    const rich7t = top10[6]
-      ? Math.floor(top10[6].bank + top10[6].cash)
-      : "nothing!";
-    const rich8t = top10[7]
-      ? Math.floor(top10[7].bank + top10[7].cash)
-      : "nothing!";
-    const rich9t = top10[8]
-      ? Math.floor(top10[8].bank + top10[8].cash)
-      : "nothing!";
-    const rich10t = top10[9]
-      ? Math.floor(top10[9].bank + top10[9].cash)
-      : "nothing!";
+    try {
+      const top10 = await User.find({}).sort({ coins: -1 }).limit(10);
+      const rich1t = top10[0]
+        ? Math.floor(top10[0].bank + top10[0].cash)
+        : "nothing!";
+      const rich2t = top10[1]
+        ? Math.floor(top10[1].bank + top10[1].cash)
+        : "nothing!";
+      const rich3t = top10[2]
+        ? Math.floor(top10[2].bank + top10[2].cash)
+        : "nothing!";
+      const rich4t = top10[3]
+        ? Math.floor(top10[3].bank + top10[3].cash)
+        : "nothing!";
+      const rich5t = top10[4]
+        ? Math.floor(top10[4].bank + top10[4].cash)
+        : "nothing!";
+      const rich6t = top10[5]
+        ? Math.floor(top10[5].bank + top10[5].cash)
+        : "nothing!";
+      const rich7t = top10[6]
+        ? Math.floor(top10[6].bank + top10[6].cash)
+        : "nothing!";
+      const rich8t = top10[7]
+        ? Math.floor(top10[7].bank + top10[7].cash)
+        : "nothing!";
+      const rich9t = top10[8]
+        ? Math.floor(top10[8].bank + top10[8].cash)
+        : "nothing!";
+      const rich10t = top10[9]
+        ? Math.floor(top10[9].bank + top10[9].cash)
+        : "nothing!";
 
-    const btnRow = new MessageActionRow().addComponents(
-      new MessageButton().setCustomId(`p1`).setLabel("⬅️").setStyle("SUCCESS"),
-      new MessageButton().setCustomId(`p2`).setLabel("➡️").setStyle("SUCCESS")
-    );
-    const finishedBtnRow = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setCustomId(`p1`)
-        .setLabel("⬅️")
-        .setStyle("SUCCESS")
-        .setDisabled(true),
-      new MessageButton()
-        .setCustomId(`p2`)
-        .setLabel("➡️")
-        .setStyle("SUCCESS")
-        .setDisabled(true)
-    );
+      const btnRow = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId(`p1`)
+          .setLabel("⬅️")
+          .setStyle("SUCCESS"),
+        new MessageButton().setCustomId(`p2`).setLabel("➡️").setStyle("SUCCESS")
+      );
+      const finishedBtnRow = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId(`p1`)
+          .setLabel("⬅️")
+          .setStyle("SUCCESS")
+          .setDisabled(true),
+        new MessageButton()
+          .setCustomId(`p2`)
+          .setLabel("➡️")
+          .setStyle("SUCCESS")
+          .setDisabled(true)
+      );
 
-    const richEmbed1 = new MessageEmbed()
-      .setTitle("🤑rainy's richest users🤑")
-      .setColor("GOLD")
-      .setFooter("economy by rainy")
-      .setTimestamp().setDescription(`
+      const richEmbed1 = new MessageEmbed()
+        .setTitle("🤑rainy's richest users🤑")
+        .setColor("GOLD")
+        .setFooter("economy by rainy")
+        .setTimestamp().setDescription(`
         ***1.*** <@${top10[0].id}>
         **💵 Total:** ${rich1t}
 
@@ -86,11 +90,11 @@ module.exports = {
         **💵 Total:** ${rich5t}
         `);
 
-    const richEmbed2 = new MessageEmbed()
-      .setTitle("🤑rainy's richest users🤑")
-      .setColor("GOLD")
-      .setFooter("economy by rainy")
-      .setTimestamp().setDescription(`
+      const richEmbed2 = new MessageEmbed()
+        .setTitle("🤑rainy's richest users🤑")
+        .setColor("GOLD")
+        .setFooter("economy by rainy")
+        .setTimestamp().setDescription(`
         ***6.*** <@${top10[6] ? top10[6].id : "Nobody!"}>
         **💵 Total:** ${rich6t}
 
@@ -107,38 +111,83 @@ module.exports = {
         **💵 Total:** ${rich10t}
         `);
 
-    const finishedEmbed = new MessageEmbed()
-      .setTitle("❌Times Up!❌")
-      .setDescription(
-        "sorry, we have disabled the buttons on this message to save bandwidth! you can always just run the command again. \n PSA: the buttons last 15 seconds!"
-      )
-      .setColor("RED");
+      const finishedEmbed = new MessageEmbed()
+        .setTitle("❌Times Up!❌")
+        .setDescription(
+          "sorry, we have disabled the buttons on this message to save bandwidth! you can always just run the command again. \n PSA: the buttons last 15 seconds!"
+        )
+        .setColor("RED");
 
-    message.reply({ embeds: [richEmbed1], components: [btnRow] }).then(msg => {
-        setTimeout(() => msg.delete(), 15000)
-    })
-
-    const filter = (i) => ["p1", "p2"].includes(i.customId);
-    const collector = message.channel.createMessageComponentCollector({
-      filter,
-      time: 1000 * 15,
-    });
-    collector.on("collect", async (i) => {
-      if (i.customId === "p1") {
-        await i.update({
-          embeds: [richEmbed1],
-          components: [btnRow],
+      message
+        .reply({ embeds: [richEmbed1], components: [btnRow] })
+        .then((msg) => {
+          setTimeout(() => msg.delete(), 15000);
         });
-      } else if (i.customId === "p2") {
-        await i.update({
-          embeds: [richEmbed2],
-          components: [btnRow],
-        });
-      }
-    });
 
-    collector.on("end", async (i) => {
-      message.reply("**time expired for buttons**");
-    });
+      const filter = (i) => ["p1", "p2"].includes(i.customId);
+      const collector = message.channel.createMessageComponentCollector({
+        filter,
+        time: 1000 * 15,
+      });
+      collector.on("collect", async (i) => {
+        if (i.customId === "p1") {
+          await i.update({
+            embeds: [richEmbed1],
+            components: [btnRow],
+          });
+        } else if (i.customId === "p2") {
+          await i.update({
+            embeds: [richEmbed2],
+            components: [btnRow],
+          });
+        }
+      });
+      collector.on("end", async (i) => {
+        message.reply("**time expired for buttons**");
+      });
+    } catch (err) {
+      client.logger.error(`Ran into an error while executing ${data.cmd.name}`);
+      console.log(err);
+      const { WebhookClient, MessageEmbed } = require("discord.js");
+      const { Webhooks } = require("../../config.json");
+      const currentDate = new Date();
+      const keygen = require("keygen");
+      const errKey = keygen.url(10);
+      const errorLog = new WebhookClient({
+        url: Webhooks.errors,
+      });
+      const devEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(`**Error:**\n\n${err}\n`)
+        .addFields(
+          { name: "Command:", value: data.cmd.name },
+          { name: "Error Key:", value: `\`${errKey}\`` },
+          {
+            name: "Guild:",
+            value: `Name: ${message.guild.name} | ID: ${message.guild.id}`,
+            inline: true,
+          },
+          {
+            name: "Author:",
+            value: `Name: ${message.author.tag} | ID: ${message.author.id}`,
+            inline: true,
+          },
+          {
+            name: "Created:",
+            value: `<t:${parseInt(message.createdTimestamp / 1000)}:R>`,
+          }
+        )
+        .setColor("RED");
+
+      const userEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(
+          `An error has occured running this command. Please DM <@${ownerid}> with the following error key: \`${errKey}\``
+        )
+        .setColor("RED");
+      message.reply({ embeds: [userEmbed] });
+      errorLog.send({ embeds: [devEmbed] });
+      return;
+    }
   },
 };
