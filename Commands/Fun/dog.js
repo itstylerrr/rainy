c664cdce9d9397fa2300885a3cea73a34b35f168
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-05-23 19:44:21 -0500

    😁 add err logging to dog cmd

--- Commands/Fun/dog.js
+++ Commands/Fun/dog.js
@@ -1,43 +1,77 @@
-const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch')
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
-    name: "dog",
-    usage: ["Get a cute picture of a dog!```{prefix}dog ```"],
-    enabled: true,
-    aliases: ["dogs"],
-    category: "Fun",
-    memberPermissions: [],
-    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-    //Settings for command
-    nsfw: false,
-    ownerOnly: false,
-    cooldown: 5000,
+  name: "dog",
+  usage: ["Get a cute picture of a dog!```{prefix}dog ```"],
+  enabled: true,
+  aliases: ["dogs"],
+  category: "Fun",
+  memberPermissions: [],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  //Settings for command
+  nsfw: false,
+  ownerOnly: false,
+  cooldown: 5000,
 
-    // Execute contains content for the command
-    async execute(client, message, args, data){
-        try{
-            const res = await fetch('https://dog.ceo/api/breeds/image/random');
-            const img = (await res.json()).message;
-            const dogEmbed = new MessageEmbed()
-            .setTitle('🐶  Woof!  🐶')
-            .setImage(img)
-            .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
-            .setColor(message.guild.me.displayHexColor)
-            message.reply({ embeds: [dogEmbed] })
-        }catch(err){
-            client.logger.error(`Ran into an error while executing ${data.cmd.name}`)
-            console.log(err)
-            return client.embed.send(message, {
-                description: `An issue has occured while running the command. If this error keeps occuring please contact our development team.`,
-                color: `RED`,
-                author: {
-                    name: `Uh Oh!`,
-                    icon_url: `${message.author.displayAvatarURL()}`,
-                    url: "",
-                }
-            });
-        }
+  // Execute contains content for the command
+  async execute(client, message, args, data) {
+    try {
+      const res = await fetch("https://dog.ceo/api/breeds/image/random");
+      const img = (await res.json()).message;
+      const dogEmbed = new MessageEmbed()
+        .setTitle("🐶  Woof!  🐶")
+        .setImage(img)
+        .setFooter(
+          message.member.displayName,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .setTimestamp()
+        .setColor(message.guild.me.displayHexColor);
+      message.reply({ embeds: [dogEmbed] });
+    } catch (err) {
+      client.logger.error(`Ran into an error while executing ${data.cmd.name}`);
+      console.log(err);
+      const { WebhookClient, MessageEmbed } = require("discord.js");
+      const { Webhooks } = require("../../config.json");
+      const currentDate = new Date();
+      const keygen = require("keygen");
+      const errKey = keygen.url(10);
+      const errorLog = new WebhookClient({
+        url: Webhooks.errors,
+      });
+      const devEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(`**Error:**\n\n${err}\n`)
+        .addFields(
+          { name: "Command:", value: data.cmd.name },
+          { name: "Error Key:", value: `\`${errKey}\`` },
+          {
+            name: "Guild:",
+            value: `Name: ${message.guild.name} | ID: ${message.guild.id}`,
+            inline: true,
+          },
+          {
+            name: "Author:",
+            value: `Name: ${message.author.tag} | ID: ${message.author.id}`,
+            inline: true,
+          },
+          {
+            name: "Created:",
+            value: `<t:${parseInt(message.createdTimestamp / 1000)}:R>`,
+          }
+        )
+        .setColor("RED");
+
+      const userEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(
+          `An error has occured running this command. Please DM <@${ownerid}> with the following error key: \`${errKey}\``
+        )
+        .setColor("RED");
+      message.reply({ embeds: [userEmbed] });
+      errorLog.send({ embeds: [devEmbed] });
+      return;
     }
-}
+  },
+};
