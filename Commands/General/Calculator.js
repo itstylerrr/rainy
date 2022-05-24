ba6c927a22afc2175cfa7edb73bf1d792eb75c15
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-05-23 19:46:55 -0500

    🧬 add err logging to calc cmd

--- Commands/General/Calculator.js
+++ Commands/General/Calculator.js
@@ -1,22 +1,70 @@
 const simplydjs = require("simply-djs");
 
 module.exports = {
-    name: "calculator",
-    usage: ["Do basic calculations```{prefix}calculator <operation>```", "Examples:```5 * 8\n3 / 3\n8 ~ 2```"],
-    enabled: true,
-    aliases: ["calc"],
-    category: "General",
-    memberPermissions: [],
-    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-    //Settings for command
-    nsfw: false,
-    ownerOnly: false,
-    cooldown: 5000,
+  name: "calculator",
+  usage: [
+    "Do basic calculations```{prefix}calculator <operation>```",
+    "Examples:```5 * 8\n3 / 3\n8 ~ 2```",
+  ],
+  enabled: true,
+  aliases: ["calc"],
+  category: "General",
+  memberPermissions: [],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  //Settings for command
+  nsfw: false,
+  ownerOnly: false,
+  cooldown: 5000,
 
-    // Execute contains content for the command
-    async execute(client, message, args, data){
-        simplydjs.calculator(message, {
-            embedColor: "a0bff6"
-          });
+  // Execute contains content for the command
+  async execute(client, message, args, data) {
+    try {
+      simplydjs.calculator(message, {
+        embedColor: "a0bff6",
+      });
+    } catch (err) {
+      client.logger.error(`Ran into an error while executing ${data.cmd.name}`);
+      console.log(err);
+      const { WebhookClient, MessageEmbed } = require("discord.js");
+      const { Webhooks } = require("../../config.json");
+      const currentDate = new Date();
+      const keygen = require("keygen");
+      const errKey = keygen.url(10);
+      const errorLog = new WebhookClient({
+        url: Webhooks.errors,
+      });
+      const devEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(`**Error:**\n\n${err}\n`)
+        .addFields(
+          { name: "Command:", value: data.cmd.name },
+          { name: "Error Key:", value: `\`${errKey}\`` },
+          {
+            name: "Guild:",
+            value: `Name: ${message.guild.name} | ID: ${message.guild.id}`,
+            inline: true,
+          },
+          {
+            name: "Author:",
+            value: `Name: ${message.author.tag} | ID: ${message.author.id}`,
+            inline: true,
+          },
+          {
+            name: "Created:",
+            value: `<t:${parseInt(message.createdTimestamp / 1000)}:R>`,
+          }
+        )
+        .setColor("RED");
+
+      const userEmbed = new MessageEmbed()
+        .setTitle("⛈️ Rainy | Errors ⛈️")
+        .setDescription(
+          `An error has occured running this command. Please DM <@${ownerid}> with the following error key: \`${errKey}\``
+        )
+        .setColor("RED");
+      message.reply({ embeds: [userEmbed] });
+      errorLog.send({ embeds: [devEmbed] });
+      return;
     }
-}
+  },
+};
